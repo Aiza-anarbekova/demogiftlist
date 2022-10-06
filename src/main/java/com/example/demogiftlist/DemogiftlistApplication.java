f59698c2e52a36f69f563cd7afddafa42d23cc0a
Aizada <aiza0000123@gmail.com>
2022-10-06 15:07:15 +0600

    demo

---
+++ src/main/java/com/example/demogiftlist/DemogiftlistApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogiftlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogiftlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogiftlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demogiftlist/DemogiftlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogiftlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogiftlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
