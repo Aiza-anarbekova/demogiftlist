5a710fc529514025d1712e49866e98f1b69462dd
Admin <sunatbekd@gmail.ru>
2022-10-10 10:46:24 +0300

    I added packages dto,repository,service,security, controller and exceptions.

---
+++ src/main/java/com/example/demogiftlist/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demogiftlist.repository;
+
+import com.example.demogiftlist.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
